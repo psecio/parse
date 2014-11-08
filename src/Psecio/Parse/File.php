a91efd6b649be7b9e171dd205de4333499b230cb
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-08 07:47:23 -0600

    Adding docblocks to the File class

--- src/Psecio/Parse/File.php
+++ src/Psecio/Parse/File.php
@@ -4,26 +4,59 @@ namespace Psecio\Parse;
 
 class File
 {
+    /**
+     * Path to file for object
+     * @var string
+     */
     private $path;
+
+    /**
+     * File contents
+     * @var string
+     */
     private $contents;
-    private $status = true;
-    private $errors = array();
+
+    /**
+     * Matches found for path evaluation
+     * @var array
+     */
     private $matches = array();
 
+    /**
+     * Init the object and set the path
+     *
+     * @param string $path Path to file
+     */
     public function __construct($path)
     {
         $this->setPath($path);
     }
 
+    /**
+     * Set the current instance file path
+     *
+     * @param string $path File path
+     */
     public function setPath($path)
     {
         $this->path = $path;
     }
+
+    /**
+     * Get the current file path
+     *
+     * @return string Current file path
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * Get the file contents
+     *
+     * @return string File contents
+     */
     public function getContents()
     {
         if ($this->contents === null) {
@@ -31,11 +64,23 @@ class File
         }
         return $this->contents;
     }
+
+    /**
+     * Set the current instance "contents" value
+     *
+     * @param string $contents File contents
+     */
     public function setContents($contents)
     {
         $this->contents = $contents;
     }
 
+    /**
+     * Add a node instance to match on file
+     *
+     * @param object $match Node instance
+     * @param string $path Path (DSL) matched against
+     */
     public function addMatch($match, $path)
     {
         $this->matches[] = array(
@@ -43,6 +88,12 @@ class File
             'node' => $match
         );
     }
+
+    /**
+     * Get the full list of matches on File instance
+     *
+     * @return array Matches set
+     */
     public function getMatches()
     {
         return $this->matches;
