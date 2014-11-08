ecd46be0c900a289b667db7af4f9c5fb6f89b89f
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-08 07:40:26 -0600

    adding docblocks all over the MatchPath and removing some unused params

--- src/Psecio/Parse/MatchPath.php
+++ src/Psecio/Parse/MatchPath.php
@@ -4,29 +4,65 @@ namespace Psecio\Parse;
 
 class MatchPath
 {
+    /**
+     * Current "path" to evaluate/locate
+     * @var string
+     */
     private $path;
+
+    /**
+     * Matches found for the path given
+     * @var array
+     */
     private $matches = array();
 
+    /**
+     * Set the path for the current evaluation
+     *
+     * @param string $path DSL formatted path
+     */
     public function setPath($path)
     {
         $this->path = $path;
     }
 
+    /**
+     * Get the current path for evaluation
+     *
+     * @return string Current DSL path
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * Add a new node object as a match
+     *
+     * @param object $node Node instance
+     */
     public function addMatch($node)
     {
         $this->matches[] = $node;
     }
 
+    /**
+     * Get the full list of matches found
+     *
+     * @return array Matches set
+     */
     public function getMatches()
     {
         return $this->matches;
     }
 
+    /**
+     * Starting with the node given, iterate through and find matches
+     *
+     * @param object $node Node instance
+     * @param string $path Path to locate/evaluate
+     * @param \Psecio\Parse\File $file File class instance
+     */
     public function evaluate($node, $path = null, &$file)
     {
         if ($path !== null) {
@@ -34,10 +70,15 @@ class MatchPath
         }
         $path = $this->getPath();
         $parts = explode('->', $path);
-        $lvl = 0;
-        return $this->iterate($node, $parts, $file, $path, $lvl);
+        $this->iterate($node, $parts, $file, $path);
     }
 
+    /**
+     * Parse the path configuration down to pieces
+     *
+     * @param string $parts Configuration string
+     * @return array Parsed configuration data
+     */
     public function formatConfig($parts)
     {
         $params = array();
@@ -75,7 +116,16 @@ class MatchPath
         return $config;
     }
 
-    public function iterate($node, $parts, &$file, $path, $lvl)
+    /**
+     * Recursive function, handles the path evaluation
+     *     and adding matches
+     *
+     * @param object $node Node instance
+     * @param array $parts Broken down parts of config/path string
+     * @param \Psecio\Parse\File $file  File instance
+     * @param string $path Path to search for
+     */
+    public function iterate($node, $parts, &$file, $path)
     {
         $config = $this->formatConfig($parts[0]);
         $m = $this->isMatch($node, $config);
@@ -88,7 +138,7 @@ class MatchPath
             $stmts = $node->stmts;
             if (!empty($stmts)) {
                 foreach ($stmts as $stmt) {
-                    $this->iterate($stmt, $parts, $file, $path, $lvl);
+                    $this->iterate($stmt, $parts, $file, $path);
                 }
             } else {
                 return $m;
@@ -99,6 +149,13 @@ class MatchPath
         }
     }
 
+    /**
+     * Evaluate if the node given is a match for the config
+     *
+     * @param object $node Node instance
+     * @param array $config Configuration to match against
+     * @return boolean Pass/fail of match
+     */
     public function isMatch($node, $config)
     {
         if (method_exists($this, $config['prefix']) === true) {
@@ -107,6 +164,13 @@ class MatchPath
         return false;
     }
 
+    /**
+     * Check the "type" of the node (node class)
+     *
+     * @param object $node Node instance
+     * @param array $data Configuration data
+     * @return boolean Pass/fail of evaluation
+     */
     public function type($node, $data)
     {
         $parts = explode('.', $data['type']);
@@ -115,6 +179,15 @@ class MatchPath
         return (stristr(get_class($node), $matchClass) !== false);
     }
 
+    /**
+     * Check the function information on node:
+     *     - Name
+     *     - Argument evaluation
+     *
+     * @param object $node Node instance
+     * @param array $data Configuration data
+     * @return boolean Pass/fail of function evaluation
+     */
     public function func($node, $data)
     {
         $matchClass = 'PhpParser\\Node\\Expr\\FuncCall';
