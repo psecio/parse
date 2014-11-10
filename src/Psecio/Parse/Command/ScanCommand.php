1cc003f8af9612b296c5894598a4ba9e02a9d5a3
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-10 07:51:09 -0500

    adding parameter handling and new options to scan command

--- src/Psecio/Parse/Command/ScanCommand.php
+++ src/Psecio/Parse/Command/ScanCommand.php
@@ -15,6 +15,8 @@ class ScanCommand extends Command
 			->setDescription('Scan the given location for possible security issues')
             ->setDefinition(array(
                 new InputOption('target', 'target', InputOption::VALUE_REQUIRED, 'Path to the file/directory'),
+                new InputOption('output', 'output', InputOption::VALUE_OPTIONAL, 'Output method [xml]'),
+                new InputOption('debug', 'debug', InputOption::VALUE_NONE, 'Show debug output'),
             ))
             ->setHelp(
                 'Scan the given location for possible security issues'
@@ -31,17 +33,27 @@ class ScanCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	$matches = array(
-    		'type:expr.eval'
-    	);
-    	$target = $input->getOption('target');
-    	if ($target == null) {
-    		throw new \Exception('Target directory/file path required! None given...');
-    	}
+		$matches = array(
+			'type:expr.eval'
+		);
+		$output = $input->getOption('output');
+		if ($output == null) {
+			$output = 'xml';
+		}
+		$target = $input->getOption('target');
+		if ($target == null) {
+			throw new \Exception('Target directory/file path required! None given...');
+		}
+		$debug = $input->getOption('debug');
 
-    	$scanner = new \Psecio\Parse\Scanner($target);
-    	$results = $scanner->execute($matches);
+		$scanner = new \Psecio\Parse\Scanner($target);
+		$results = $scanner->execute($matches);
 
-    	print_r($results);
+		if ($debug !== null) {
+			print_r($results);
+		}
+
+		$xml = new \Psecio\Parse\Output\Xml();
+		echo $xml->generate($results);
     }
 }
