2c79e96c468668404a9419ea5d0d725e86609df7
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-11 12:17:52 -0500

    adding checks for output methods that use variables in their options/output

---
+++ src/Psecio/Parse/Tests/TestOutputWithVariable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Psecio\Parse\Tests;
+
+/**
+ * Check for output functions that use a variable in the output
+ */
+class TestOutputWithVariable extends \Psecio\Parse\Test
+{
+	private $outputFunctions = array(
+		'print_r', 'printf', 'vprintf', 'sprintf'
+	);
+
+	public function evaluate($node, $file = null)
+	{
+		// See if our echo or print (constructs) uses concat
+		if ($node instanceof \PhpParser\Node\Stmt\Echo_ || $node instanceof PhpParser\Node\Expr\Print_) {
+			if (isset($node->exprs[0]) && $node->exprs[0] instanceof PhpParser\Node\Expr\BinaryOp\Concat) {
+				return false;
+			}
+		}
+
+		// See if our other output functions use concat
+		if ($node instanceof \PhpParser\Node\Expr\FuncCall && in_array($node->name, $this->outputFunctions)) {
+			if (isset($node->args[0]) && $node->args[0]->value instanceof \PhpParser\Node\Expr\BinaryOp\Concat) {
+				return false;
+			}
+		}
+	}
+}
