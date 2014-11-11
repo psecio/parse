e0e92e1aa52d8ab1b38b5e8f87ca0de6bc3f9b79
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-11 10:55:27 -0500

    making the extract test work - woo!

--- src/Psecio/Parse/Tests/TestExtractNotOverwrite.php
+++ src/Psecio/Parse/Tests/TestExtractNotOverwrite.php
@@ -2,12 +2,29 @@
 
 namespace Psecio\Parse\Tests;
 
+/**
+ * For the extract function, if either:
+ * 	- the second param is not set (overwrite by default)
+ *  = the second param is set but is EXTR_OVERWRITE
+ * fail...
+ */
 class TestExtractNotOverwrite extends \Psecio\Parse\Test
 {
 	public function evaluate($node)
 	{
 		if (get_class($node) == "PhpParser\\Node\\Expr\\FuncCall" && $node->name == 'extract') {
-			echo $node->name."\n";
+
+			// Check to be sure it has two arguments
+			if (count($node->args) < 2) {
+				return false;
+			}
+
+			// So we have two parameters...see if #2 is not equal to EXTR_OVERWRITE
+			if ($node->args[1]->value->name->parts[0] === 'EXTR_OVERWRITE') {
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
