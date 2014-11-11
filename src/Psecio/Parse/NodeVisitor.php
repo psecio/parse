0722887b731ff4127f58f35e6cca7a4378ff1e48
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-11 10:54:12 -0500

    adding logger to the NodeVisitor

--- src/Psecio/Parse/NodeVisitor.php
+++ src/Psecio/Parse/NodeVisitor.php
@@ -6,10 +6,12 @@ class NodeVisitor extends \PhpParser\NodeVisitorAbstract
 {
 	private $tests = array();
 	private $results = array();
+	private $logger;
 
-	public function __construct(\Psecio\Parse\TestCollection $tests)
+	public function __construct(\Psecio\Parse\TestCollection $tests, $logger)
 	{
 		$this->tests = $tests;
+		$this->logger = $logger;
 	}
 
 	public function getResults()
@@ -25,7 +27,9 @@ class NodeVisitor extends \PhpParser\NodeVisitorAbstract
 	public function enterNode(\PhpParser\Node $node)
 	{
 		foreach ($this->tests as $test) {
-			if ($test->evaluate($node) == true) {
+			if ($test->evaluate($node) === false) {
+				$this->logger->addInfo('oops');
+
 				$this->addResult($node);
 			}
 		}
