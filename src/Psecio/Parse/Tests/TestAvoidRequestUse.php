e2aca9eba12d42d111c05eccfb4be57d2ab5153f
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-11 15:04:29 -0500

    adding tests for $_REQUEST and mysql_real_escape checking

---
+++ src/Psecio/Parse/Tests/TestAvoidRequestUse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Psecio\Parse\Tests;
+
+/**
+ * Don't use $_REQUEST, know where your data is coming from
+ */
+class TestAvoidRequestUse extends \Psecio\Parse\Test
+{
+	public function evaluate($node, $file = null)
+	{
+		if ($node instanceof \PhpParser\Node\Expr\Variable && $node->name == '_REQUEST') {
+			return false;
+		}
+	}
+}
---
+++ src/Psecio/Parse/Tests/TestUseMysqlRealEscapeString.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Psecio\Parse\Tests;
+
+/**
+ * Don't use the mysql_real_escape_string function
+ * 	- use bound params/prepared statements instead
+ */
+class TestUseMysqlRealEscapeString extends \Psecio\Parse\Test
+{
+	public function evaluate($node, $file = null)
+	{
+		if ($node instanceof \PhpParser\Node\Expr\FuncCall && $node->name == 'mysql_real_escape_string') {
+			return false;
+		}
+		return true;
+	}
+}
