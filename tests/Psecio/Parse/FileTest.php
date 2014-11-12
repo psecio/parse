8293eeb1be99abd8921650d6e7e4d0e5a1fc6007
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-12 12:01:55 -0500

    adding phpunit config and first tests (File)

---
+++ tests/Psecio/Parse/FileTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Psecio\Parse;
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+	private $file;
+	private $path;
+
+	public function setUp()
+	{
+		$this->path = __DIR__.'/sample-file.php';
+		$this->file = new File($this->path);
+	}
+	public function tearDown()
+	{
+		unset($this->file);
+	}
+
+	/**
+	 * Test the getting/setting of the file path
+	 */
+	public function testGetSetPath()
+	{
+		$this->assertEquals(
+			$this->path,
+			$this->file->getPath()
+		);
+	}
+
+	/**
+	 * Test that the content from the file is fetched correctly
+	 */
+	public function testGetContent()
+	{
+		$this->assertEquals(
+			"<?php echo 'sample-file.php'; ?>",
+			$this->file->getContents()
+		);
+	}
+
+	/**
+	 * Test the setting of different content
+	 */
+	public function testSetContent()
+	{
+		$newContent = 'this is a test';
+		$this->file->setContents($newContent);
+
+		$this->assertEquals(
+			$this->file->getContents(),
+			$newContent
+		);
+	}
+
+	/**
+	 * Test the "get lines" funcitonality
+	 */
+	public function testGetLines()
+	{
+		$newContent = "this is\na test with\nnewlines\nhere";
+		$this->file->setContents($newContent);
+
+		// A single line w/o optional param
+		$lines = $this->file->getLines(2);
+
+		$this->assertTrue(is_array($lines) && !empty($lines));
+		$this->assertEquals(
+			$lines,
+			array('a test with')
+		);
+
+		// Multiple lines with second param
+		$this->assertEquals(
+			$this->file->getLines(2, 5),
+			array('a test with', 'newlines', 'here')
+		);
+	}
+}
---
+++ tests/Psecio/Parse/sample-file.php
@@ -0,0 +1,1 @@
+<?php echo 'sample-file.php'; ?>
