b856a78ff1ed2f67aed3cf41fe789d6fce825c4a
Chris Cornutt <enygma@phpdeveloper.org>
2014-11-12 09:34:45 -0500

    adding docblocks to NodeVisitor class

--- src/Psecio/Parse/NodeVisitor.php
+++ src/Psecio/Parse/NodeVisitor.php
@@ -4,11 +4,37 @@ namespace Psecio\Parse;
 
 class NodeVisitor extends \PhpParser\NodeVisitorAbstract
 {
+	/**
+	 * Current set of tests to execute
+	 * @var array
+	 */
 	private $tests = array();
+
+	/**
+	 * Results from test evaluation
+	 * @var array
+	 */
 	private $results = array();
+
+	/**
+	 * Logger object instance (Monolog)
+	 * @var object
+	 */
 	private $logger;
+
+	/**
+	 * Current file under evaluation
+	 * @var \Psecio\Parse\File
+	 */
 	private $file;
 
+	/**
+	 * Init the object and set up the tests, file and logger
+	 *
+	 * @param \Psecio\Parse\TestCollection $tests Set of tests in a collection
+	 * @param \Psecio\Parse\File $file File object instance
+	 * @param object $logger Logger object (Monolog)
+	 */
 	public function __construct(\Psecio\Parse\TestCollection $tests, \Psecio\Parse\File $file, $logger)
 	{
 		$this->tests = $tests;
@@ -16,16 +42,31 @@ class NodeVisitor extends \PhpParser\NodeVisitorAbstract
 		$this->file = $file;
 	}
 
+	/**
+	 * Get the current results of test evaluation
+	 *
+	 * @return array Results set
+	 */
 	public function getResults()
 	{
 		return $this->results;
 	}
 
+	/**
+	 * Add a new test result
+	 *
+	 * @param object $node Node object
+	 */
 	public function addResult($node)
 	{
 		$this->results[] = $node;
 	}
 
+	/**
+	 * Interface function called when node it first hit
+	 *
+	 * @param \PhpParser\Node $node Node instance
+	 */
 	public function enterNode(\PhpParser\Node $node)
 	{
 		foreach ($this->tests as $test) {
