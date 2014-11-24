8233a8d011ad8a2ea2c955718711f4ca18235957
Chris Cornutt <chris.cornutt@hp.com>
2014-11-24 14:40:51 -0600

    adding check to ensure display_errors isn't being enabled manually

---
+++ src/Psecio/Parse/Tests/TestDisableDisplayErrors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Psecio\Parse\Tests;
+
+/**
+ * The "display_errors" setting should not be enabled manually
+ */
+class TestDisableDisplayErrors extends \Psecio\Parse\Test
+{
+    protected $description = 'The "display_errors" setting should not be enabled manually';
+
+    public function evaluate($node, $file = null)
+    {
+        if ($node->isFunction('ini_set') === true) {
+            // see if the setting is "display_errors" && if they're enabling it
+            if ($node->args[0]->value->value == 'display_errors') {
+                $value = $node->args[1]->value;
+                print_r($value);
+
+                if ($value instanceof \PhpParser\Node\Expr\ConstFetch) {
+                    $value = $value->name->parts[0];
+                } else {
+                    $value = $node->args[1]->value->value;
+                }
+
+                if ($value == 1 || $value == true) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
