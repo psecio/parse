484b37896734ae9fa6540fd591634213fd90935e
Michael D Johnson <redbeard@mdjohnson.us>
2015-01-06 21:43:12 -0600

    Replace TestSessionRegenFalseTest with ParseTest
    TestSessionRegenFalseTest.php wasn't doing anything that couldn't be done
    using a ParseTest unit test. So, make sure the TestSessionRegenFalseParseTest
    class is up to the job and drop the original test.

--- tests/Tests/TestSessionRegenFalseParseTest.php
+++ tests/Tests/TestSessionRegenFalseParseTest.php
@@ -9,9 +9,11 @@ class TestSessionRegenFalseParseTest extends ParseTest
     public function parseSampleProvider()
     {
         return [
+            ['$x = 17;', true],
             ['session_regenerate_id();', false],
             ['session_regenerate_id(false);', false],
             ['session_regenerate_id(true);', true],
+            ['session_regenerate_id(17);', false],
             ['$x["a"]("blah");', false],
             ['random_function();', true],
             ];
