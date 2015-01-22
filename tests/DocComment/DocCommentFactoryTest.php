e7d0f65d1f9dfcdd7ed6d4b6dffc769ef07db3f8
Michael D Johnson <redbeard@mdjohnson.us>
2015-01-22 01:07:14 -0600

    Improve coverage of DocCommentFactory
    It wasn't detecting if createDocComment() was returning a DocCommentInterface.

--- tests/DocComment/DocCommentFactoryTest.php
+++ tests/DocComment/DocCommentFactoryTest.php
@@ -4,15 +4,23 @@ namespace Psecio\Parse\DocComment;
 
 class DocCommentFactoryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDocComment()
-    {
-        $comment = <<<EOD
+    const COMMENT = <<<EOD
 /**
  * Summary
  */
 EOD;
+
+    public function testCreateDocCommentReturnsDocComment()
+    {
         $factory = new DocCommentFactory;
-        $docComment = $factory->createDocComment($comment);
-        $this->assertSame($comment, $docComment->getRawComment());
+        $docComment = $factory->createDocComment(self::COMMENT);
+        $this->assertInstanceOf(__NAMESPACE__ . '\\DocCommentInterface', $docComment);
+    }
+
+    public function testCreateDocCommentSavesRawComment()
+    {
+        $factory = new DocCommentFactory;
+        $docComment = $factory->createDocComment(self::COMMENT);
+        $this->assertSame(self::COMMENT, $docComment->getRawComment());
     }
 }
